a72d1c0d6570662634bdf3d6e7ba1ebe5942a36b
Ashvini Barode <abarode7@gmail.com>
2026-07-21 20:21:40 +0530

    Add CustomUserDetailsService and PasswordEncoder configuration

--- src/main/java/com/finvision/security/service/CustomUserDetailsService.java
+++ src/main/java/com/finvision/security/service/CustomUserDetailsService.java
@@ -1,4 +1,32 @@
 package com.finvision.security.service;
 
-public class CustomUserDetailsService {
+import com.finvision.user.entity.User;
+import com.finvision.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority(user.getRole().getName().name()))
+        );
+    }
 }
